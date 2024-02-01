e3cb69f94135cc9fe19212be34c7194548ae6860
eld1111213 <122581056+eld1111213@users.noreply.github.com>
2024-02-01 13:25:35 -0600

    Update home.js

--- static/assets/scripts/home.js
+++ static/assets/scripts/home.js
@@ -8,7 +8,7 @@ try {
 
 if (!inFrame && !navigator.userAgent.includes('Firefox')) {
   const popup = open('about:blank', '_blank')
-  if (!popup || popup.closed) alert('Allow popups and redirects to avoid this from being blocked.')
+  if (!popup || popup.closed) alert('Allow popups and redirects to avoid this from being blocked [Made By Eld]')
   else {
     const doc = popup.document
     const iframe = doc.createElement('iframe')
